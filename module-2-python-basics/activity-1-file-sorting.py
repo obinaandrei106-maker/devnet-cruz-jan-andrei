5e2fd02283d348297f93c5bde376c71d870f1891
obinaandrei106-maker <obinaandrei106@gmail.com>
2026-09-25 13:29:37 +0000

    Lesson 1 DONE!

--- module-2-python-basics/activity-1-file-sorting.py
+++ module-2-python-basics/activity-1-file-sorting.py
@@ -1,7 +1,7 @@
 """
 Module 2 — Activity: File Sorting with os and shutil
-Student: [your name]
-Date: [date]
+Student: Cruz, Jan Andrei O.
+Date: September 25, 2026
 
 ============================================
 WHAT DID YOU BUILD? (explain in your own words)
